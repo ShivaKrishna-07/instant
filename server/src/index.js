9c6e1b37163ed2b734596674b1cabcdeb6015d6d
Shiva Krishna <shivakrishna6032@gmail.com>
2026-01-22 15:38:21 +0530

    fix: Update CORS configuration to allow all origins

--- server/src/index.js
+++ server/src/index.js
@@ -18,9 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [
-      process.env.CLIENT_ORIGIN || "http://localhost:3000", "https://instant-cyan-psi.vercel.app/"
-    ],
+    origin: '*',
     credentials: true,
   })
 );
